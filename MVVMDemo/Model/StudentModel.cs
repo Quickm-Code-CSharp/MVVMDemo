1c1f3526b18fd87b236869879c8062ddd4272cf1
quickm.career <quickm.career@gmail.com>
2021-04-19 21:47:19 -0700

    MVVM WPF Tutorial on Tutorialpoint, next chapter.

--- MVVMDemo/Model/StudentModel.cs
+++ MVVMDemo/Model/StudentModel.cs
@@ -55,10 +55,7 @@ namespace MVVMDemo.Model
 
         private void RaisePropertyChanged(string property)
         {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(property));
-            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
         }
     }
 }
